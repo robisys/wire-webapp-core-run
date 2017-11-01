1fc513cecaccc3facd836134f50a8b28b53053e4
Robert P <robisys@users.noreply.github.com>
2017-11-01 19:55:04 +0100

    Add files via upload

---
+++ run2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#EMAIL="wire@robisys.de"
+#PASSWORD="P#a48Gb8"
+
+#1
+#EMAIL="wire1@robisys.de"
+#PASSWORD="K79#Abd46"
+
+
+#EMAIL="name@email.com"
+#PASSWORD="password"
+
+
+
+EMAIL="wire2@robisys.de"
+PASSWORD="H84#67bK"
+
+
+node index.js --e="$EMAIL" --p="$PASSWORD"
+
+
+
+
+
