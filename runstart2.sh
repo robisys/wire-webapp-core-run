358dfaeac024188fe9488cf3d679a3f3fcd7e1c8
Robert P <robisys@users.noreply.github.com>
2017-11-01 20:04:35 +0100

    Update runstart2.sh

--- runstart2.sh
+++ runstart2.sh
@@ -8,6 +8,18 @@ PASSWORD="K79#Abd46";
 CONVERSATION_ID="1f0ad059-1ac4-46c6-bd4c-a689c789a2c2"
 
 MESSAGE="knorkel-message"
+#0
+#EMAIL="wire@robisys.de"
+#PASSWORD="P#a48Gb8"
+#1
+#EMAIL="wire1@robisys.de"
+#PASSWORD="K79#Abd46"
+#
+#EMAIL="name@email.com"
+#PASSWORD="password"
+#2
+#EMAIL="wire2@robisys.de"
+#PASSWORD="H84#67bK"
 
 node start2.js --e="$EMAIL" --p="$PASSWORD" --c="$CONVERSATION_ID"  --m="MESSAGE"
 
