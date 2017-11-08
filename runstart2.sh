52c531c357879a289a4ab58b2a62d07c1283fa66
Robert P <robisys@users.noreply.github.com>
2017-11-08 20:55:47 +0100

    Update runstart2.sh

--- runstart2.sh
+++ runstart2.sh
@@ -5,7 +5,7 @@ PASSWORD="password";
 CONVERSATION_ID="123456-1234-4567-8901-a689c789a2c2"
 
 MESSAGE="get self"
-
+echo "password  aktualisieren !!!"
 echo "email: " $EMAIL $MESSAGE 
 LOGFILE="logfile2.txt"
 echo "===================================================" >>$LOGFILE
